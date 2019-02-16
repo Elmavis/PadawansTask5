78fe8aad3cb5a2ba2a3aa638af33319f734801bc
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-16 21:43:30 +0300

    FixsStupidBug

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -2,20 +2,23 @@
 
 namespace PadawansTask5
 {
-    public static string CheckIfSymmetric(int[] source)
+    public static class ArrayHelper
     {
-        if (source == null)
-            throw new ArgumentNullException();
-        if (source.Length == 0)
-            throw new ArgumentException();
-
-        int n = source.Length;
-        for (int i = 0; i < n / 2; i++)
+        public static string CheckIfSymmetric(int[] source)
         {
-            if (source[i] != source[n - 1 - i])
-                return "No";
-        }
+            if (source == null)
+                throw new ArgumentNullException();
+            if (source.Length == 0)
+                throw new ArgumentException();
 
-        return "Yes";
+            int n = source.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (source[i] != source[n - 1 - i])
+                    return "No";
+            }
+
+            return "Yes";
+        }
     }
 }
